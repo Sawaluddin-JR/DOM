aec70d9cccf043505020704cadcd3ed8a5f3d504
Sawaluddin <sawaluddinsrg472@gmail.com>
2024-05-21 21:01:06 +0700

    Add event onFocus

--- form-event/index.js
+++ form-event/index.js
@@ -24,4 +24,10 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("notifikasiSisaKarakter").style.color = "black";
     }
   });
+
+  document.getElementById("inputNama").addEventListener("focus", function () {
+    console.log("inputNama: focus");
+    document.getElementById("notifikasiSisaKarakter").style.visibility =
+      "visible";
+  });
 });
