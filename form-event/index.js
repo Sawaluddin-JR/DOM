85bf24ad4c207a50c2415db79ac1d7e77dec0b6f
Sawaluddin <sawaluddinsrg472@gmail.com>
2024-05-21 21:04:07 +0700

    Add event onBlur

--- form-event/index.js
+++ form-event/index.js
@@ -25,9 +25,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  //event onFocus
   document.getElementById("inputNama").addEventListener("focus", function () {
     console.log("inputNama: focus");
     document.getElementById("notifikasiSisaKarakter").style.visibility =
       "visible";
+  });
+
+  //event onBlur
+  document.getElementById('inputNama').addEventListener('blur', function () {
+    console.log('inputNama: blur');
+    document.getElementById('notifikasiSisaKarakter').style.visibility = 'hidden';
   });
 });
